ddd8e577b8f5116db82af12218114d5fbd7fd7b6
Ulik <Vasy.Pupkin1999@bk.ru>
2020-09-11 21:15:13 +0600

    header finish

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    
+    //----------- Mmenu ----------//
+
+    $('.hamburger').on('click', function() {
+        $('.hamburger').toggleClass('is-active');
+        $('.menu__list').toggleClass('open-menu');
+    });
+
+
+
+});
+
+
