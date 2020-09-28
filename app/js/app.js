d7d7e4f69cf6b31227af0ba40ef79ea533e79582
Ulik <Vasy.Pupkin1999@bk.ru>
2020-09-28 17:21:21 +0600

    blog finish

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,77 @@
 $(function() {
 
     
-    //----------- Mmenu ----------//
+    //----------- header menu ----------//
 
     $('.hamburger').on('click', function() {
         $('.hamburger').toggleClass('is-active');
         $('.menu__list').toggleClass('open-menu');
+    });
+
+    //---------- button top -----------//
+
+    var btn = jQuery('.Button__top');
+
+    jQuery(window).scroll(function() {
+      if (jQuery(window).scrollTop() > 1000) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+      btn.on('click', function(e) {
+      e.preventDefault();
+      jQuery('html, body').animate({scrollTop:0}, '1000');
+    });
+    
+    var btn = jQuery('.Button__top');
+      jQuery(window).scroll(function() {
+      if (jQuery(window).scrollTop() > 1000) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+
+    //-------- slick slider ---------//
+
+    $('.features__blocks .slick').slick({
+      autoplay: true,
+      dots: true,
+      arrows: false,
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 2,
+            dots: true
+          }
+        } 
+      ]
+    })
+
+
+    //-------- sidebar ---------//
+
+    $('.sidebar__btn').on('click', () => {
+      $('.sidebar').toggleClass('sidebar-open');
+    });
+
+
+    //-------- alert button ---------//
+
+    $('.about__btn div').on('click', () => {
+      alert('Alert text!')
     });
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ let paths = {
 	scripts: {
 		src: [
 			'node_modules/jquery/dist/jquery.min.js',
+			'app/js/slick.min.js',
 			baseDir + '/js/app.js' // app.js. Always at the end
 		],
 		dest: baseDir + '/js',
